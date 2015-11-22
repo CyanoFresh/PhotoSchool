97c71d4510880c597a41d2c90384e21dcaf158ea
Alex Solomaha <cyanofresh@gmail.com>
2015-11-22 14:22:50 +0200

    added local db config

--- config/db.php
+++ config/db.php
@@ -1,9 +1,13 @@
 <?php
 
-return [
+use yii\helpers\ArrayHelper;
+
+$db =  [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=127.0.0.1;dbname=photoschool',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
 ];
+
+return ArrayHelper::merge($db, require 'db-local.php');
